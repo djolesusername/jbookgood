f376987ce83a29cf53bd2e601680607376012357
Djordje Cvetkovic <djole001@gmail.com>
2021-09-09 18:54:03 +0200

    added iframe

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm'
 import { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import { fetchPlugin } from './plugins/fetch-plugin'
 
 const App = () => {
     const ref = useRef<any>()
@@ -12,7 +13,7 @@ const [code, setCode] = useState('');
 const startService = async () => {
     ref.current= await esbuild.startService({
         worker: true, 
-        wasmURL: '/esbuild.wasm'
+        wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     })
 }
 
@@ -28,12 +29,18 @@ const onClick = async() => {
         entryPoints:  ['index.js'],
         bundle: true,
         write: false,
-        plugins: [unpkgPathPlugin(input)],
+        plugins: [unpkgPathPlugin(), 
+        fetchPlugin(input)],
         define: {'process.env.NODE_ENV': "'production'",
     global: 'window'}
 
     })
 setCode(result.outputFiles[0].text)
+try {eval(result.outputFiles[0].text)
+} catch (err) {
+    console.log(err)
+}
+
 }
 
     return <div> 
@@ -43,6 +50,7 @@ setCode(result.outputFiles[0].text)
     <button onClick={onClick}>Submit</button>
     </div>
     <pre>{code}</pre>
+    <iframe title="a45"sandbox="" src="/test.html"/> 
 
     </div> 
 }
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,13 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localForage from 'localforage';
 
-const fileCache = localForage.createInstance({
-  name: 'filecache'
-})
 
-console.log('test')
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
@@ -34,33 +28,7 @@ build.onResolve({filter: /^\.+\//}, (args: any ) => {
  
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
-//Check to see if it is in cache,
-
-const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
-
-//if so return
-if (cachedResult) {
-  return cachedResult
-}
-
-        const { data, request } = await axios.get(args.path);
-        const result: esbuild.OnLoadResult =  {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL ('./', request.responseURL).pathname
-        };
-        await fileCache.setItem(args.path, result)
-        return result
-      });
+     
     },
   };
 };
